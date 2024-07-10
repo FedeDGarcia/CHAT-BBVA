9c3b8951d72488f0c43f558badd83b429447dd83
matias <peluche@Peluches-MBP.fibertel.com.ar>
2024-07-10 11:43:20 -0300

    typos y fixes en la funcion fin

--- app.py
+++ app.py
@@ -2,32 +2,32 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import yaml
-import pandas
+import pandas as pd
 
-app = fastAPI()
+app = FastAPI()
 
 with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
 mensajes = config['mensajes']
 
-class ActualState:
+class ActualState(BaseModel):
     nodo: int
     mensaje: str
 
 def fin(parametro):
-    pass
+    return 'fin'
 
 def verificar_dni(dni: str):
-    dni = int(str)
+    dni = int(dni)
     dnis = pd.read_excel(config['planilla'])
     return dni in dnis['DNI']
 
 @app.post('/respuesta')
 async def respuesta(state: ActualState):
     funcion = mensajes[state.nodo]['siguientes']['funcion']
-    decision = funcion(state.mensaje)
-    proximo_nodo = mensajes[state.nodo]['siguientes'][decision]
+    decision = eval(funcion)(state.mensaje)
+    proximo_nodo = mensajes[state.nodo]['siguientes']['resultados'][decision]
     texto = mensajes[proximo_nodo]['texto']
     return texto
 
