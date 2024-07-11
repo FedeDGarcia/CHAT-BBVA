2064239b7a04d6caefb017719ed7f4e172323423
matias <matiasmillasson@gmail.com>
2024-07-11 11:25:59 -0300

    agrego el nodo 3

--- app.py
+++ app.py
@@ -10,25 +10,34 @@ with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
 mensajes = config['mensajes']
+dnis = pd.read_excel(config['planilla'])
 
 class ActualState(BaseModel):
     nodo: int
     mensaje: str
+
+def pasar(parametro):
+    return 'pasar'
 
 def fin(parametro):
     return 'fin'
 
 def verificar_dni(dni: str):
     dni = int(dni)
-    dnis = pd.read_excel(config['planilla'])
     return dni in dnis['DNI'].values
+
+def dame_nombre(dni: str):
+    dni = int(dni)
+    return dnis[dnis['DNI'] == dni]['NOMBRE'].values[0]
 
 @app.post('/respuesta')
 async def respuesta(state: ActualState):
     funcion = mensajes[state.nodo]['siguientes']['funcion']
     decision = eval(funcion)(state.mensaje)
     proximo_nodo = mensajes[state.nodo]['siguientes']['resultados'][decision]
-    texto = mensajes[proximo_nodo]['texto']
+    nombre = eval(mensajes[proximo_nodo]['funcion'])(state.mensaje)
+    print(nombre)
+    texto = mensajes[proximo_nodo]['texto'].format(nombre)
     return texto
 
 if __name__ == '__main__':
