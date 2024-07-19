822b80924136a6aa53b95394f20379c593b827c6
matiasmilla <matiasmillasson@gmail.com>
2024-07-19 15:59:24 -0300

    fixeo typos en los tests del nodo 1

--- tests.py
+++ tests.py
@@ -35,12 +35,12 @@ class Nodo1(unittest.TestCase):
     def test_dni_valido(self):
         payload = {'nodo': 1, 'mensaje': dni_valido}
         response = requestAPI(payload)
-        self.assertEqual(response, 'ACOSTA ARRIETA FEDERICO AGUSTI Un gusto saludarte! Te pedimos que nos facilites un correo electrónico para continuar la gestión')
+        self.assertEqual(response, 'URO ROCIO MARCELA Un gusto saludarte! Te pedimos que nos facilites un correo electrónico para continuar la gestión')
 
     def test_dni_invalido(self):
         payload = {'nodo': 1, 'mensaje': dni_invalido}
         response = requestAPI(payload)
-        self.assertEqual(response, 'Al momento en CDN no tenemos asignada tu deuda por favor contactate con BBVA al 0800-999-2282 de lunes a viernes de 10 a 15 hs')
+        self.assertEqual(response, 'Al momento en CDN no tenemos asignada tu deuda, por favor contactate con BBVA al 0800-999-2282 DE LUNES A VIERNES DE 10 A 15 HS')
 
 class Nodo2(unittest.TestCase):
     def test_terminar(self):
